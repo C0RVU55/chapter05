f9f69466c3d7b8845e2f91711a7f1db0ad480f45
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-12-10 18:07:45 +0900

    Input_Output_Stream

--- src/com/javaex/ex03/PhoneApp.java
+++ src/com/javaex/ex03/PhoneApp.java
@@ -1,10 +1,36 @@
 package com.javaex.ex03;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PhoneApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		// 텍스트파일 불러오기 > 배열 split > 리스트관리 및 출력 > 데이터추가(리스트3에 add로 1추가해서 전체를 다시 쓰는 방식)
+		// 이름: 핸드폰: 회사:
 
+		Reader in = new FileReader("C:\\javaStudy\\PhoneDB.txt");
+		BufferedReader br = new BufferedReader(in);
+
+		// 추가???
+
+		while (true) {
+			String str = br.readLine();
+			if (str == null) {
+				break;
+			}
+			String[] sArr = str.split(",");
+			System.out.println("이름: " + sArr[0]);
+			System.out.println("핸드폰: " + sArr[1]);
+			System.out.println("회사: " + sArr[2]);
+			System.out.println("");
+		}
+
+		br.close();
 	}
 
 }
--- src/com/javaex/ex04/PhoneApp.java
+++ src/com/javaex/ex04/PhoneApp.java
@@ -1,10 +1,47 @@
 package com.javaex.ex04;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PhoneApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		// I/O Stream 과제
+		// 텍스트파일 불러오기 > 배열 split > 리스트관리 및 출력 > 데이터추가(리스트3에 add로 1추가해서 전체를 다시 쓰는 방식)
+		// 이름: 핸드폰: 회사:
 
+		Reader in = new FileReader("C:\\javaStudy\\PhoneDB.txt");
+		BufferedReader br = new BufferedReader(in);
+
+		List<Person> pList = new ArrayList<Person>();
+
+		while (true) {
+			String p = br.readLine();
+			if (p == null) {
+				break;
+			}
+			String[] pArr = p.split(","); // 이걸 조건문 전에 썼는데 NullPoint오류남. null을 배열에 넣을 수 없어서 그런듯.
+			Person person = new Person(pArr[0], pArr[1], pArr[2]);
+			pList.add(person);
+		}
+		for (int i = 0; i < pList.size(); i++) {
+			pList.get(i).showInfo();
+		}
+
+		// 추가
+		System.out.println("-----데이터 추가-----");
+		
+		Person pp = new Person("이다현", "000-0000-0000", "123-123123-22");
+		pList.add(pp);
+		for (int i = 0; i < pList.size(); i++) {
+			pList.get(i).showInfo();
+		}
+
+		br.close();
 	}
 
 }
